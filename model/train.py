6347d05be828983245e0848c66aa9255141b28f5
tikkikkit21 <tikkikkit21@gmail.com>
2023-11-28 23:00:06 -0500

    Use command line arguments to set number of epochs

--- model/train.py
+++ model/train.py
@@ -1,7 +1,17 @@
-from ultralytics import YOLO
-
+# parse program arguments
+import sys
 EPOCHS = 20
 IMGSZ=640
+
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-h":
+        print("Usage: train.py [num epochs, default is 20]")
+        exit()
+    else:
+        EPOCHS = sys.argv[1]
+
+# train with YOLOv8
+from ultralytics import YOLO
 
 model = YOLO("yolov8n.pt")
 training_results = model.train(
