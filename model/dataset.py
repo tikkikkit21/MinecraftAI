3be275818a6d21be64ab8a1bc43df822de7cf2b2
tikkikkit21 <tikkikkit21@gmail.com>
2023-11-29 10:48:14 -0500

    Download latest version

--- model/dataset.py
+++ model/dataset.py
@@ -11,4 +11,22 @@ ROBOFLOW_PROJECT = os.environ.get("ROBOFLOW_PROJECT")
 from roboflow import Roboflow
 rf = Roboflow(ROBOFLOW_API)
 project = rf.workspace(ROBOFLOW_WORKSPACE).project(ROBOFLOW_PROJECT)
-dataset = project.version(1).download(model_format="yolov8", location="./dataset")
+
+# get version number
+import sys
+
+PROJECT_VERSION = 1
+if len(sys.argv) == 2:
+    if sys.argv[1] == "-h":
+        print("Usage: dataset.py [version number, default is latest]")
+        exit()
+    else:
+        try:
+            PROJECT_VERSION = int(sys.argv[1])
+        except ValueError:
+            print("Version number needs to be an int")
+            exit()
+else:
+    PROJECT_VERSION = project.versions()[0].version
+    
+dataset = project.version(PROJECT_VERSION).download(model_format="yolov8", location="./dataset")
