5d85c690b378c43210be11564e7bf2b69ec3d91c
tikkikkit21 <tikkikkit21@gmail.com>
2023-11-29 12:21:18 -0500

    Change color for class

--- model/webcam.py
+++ model/webcam.py
@@ -4,7 +4,7 @@ import math
 import time
 
 # YOLO set up
-path_to_weights = "./runs/detect/train10/weights/best.pt"
+path_to_weights = "./runs/detect/train2/weights/best.pt"
 model = YOLO(path_to_weights)
 
 # 0 indicates it's a webcam
@@ -14,7 +14,10 @@ capture = cv2.VideoCapture(0)
 capture.set(3, 640)
 capture.set(4, 480)
 
-classNames = ["walk"]
+classes = [
+    ('stand', (0, 0, 255)),
+    ('walk', (0, 255, 0))
+]
 
 while True:
     success, img= capture.read()
@@ -25,29 +28,24 @@ while True:
         boxes = r.boxes
 
         for box in boxes:
+            # get class index and confidence
+            (className, classColor) = classes[int(box.cls[0])]
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+
             # bounding box
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # convert to int values
 
             # put box in cam
-            cv2.rectangle(img, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-            # confidence
-            confidence = math.ceil((box.conf[0]*100))/100
-            # print("Confidence --->",confidence)
-
-            # class name
-            cls = int(box.cls[0])
-            # print("Class name -->", classNames[cls])
+            cv2.rectangle(img, (x1, y1), (x2, y2), classColor, 2)
 
             # object details
             org = [x1, y1]
             font = cv2.FONT_HERSHEY_SIMPLEX
             fontScale = 1
-            color = (0, 0, 255)
             thickness = 2
 
-            cv2.putText(img, f"{classNames[cls]}: {confidence}", org, font, fontScale, color, thickness)
+            cv2.putText(img, f"{className}: {confidence}", org, font, fontScale, classColor, thickness)
 
     # names the window
     cv2.imshow('Webcam', img)
